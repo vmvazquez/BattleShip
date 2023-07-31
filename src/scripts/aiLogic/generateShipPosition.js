097ec7953a799c8cbd93659bb641d082fd6d3ee6
vmvazquez <victorv1996@yahoo.com>
2023-07-31 07:40:07 -0700

    added logic to give ships a an outer shell where others ships cannot be placed

--- src/scripts/aiLogic/generateShipPosition.js
+++ src/scripts/aiLogic/generateShipPosition.js
@@ -39,21 +39,15 @@ const generateRandomShipLocation = (shipHealth) => {
 
     switch (direction) {
       case 0:
-        //   console.log('north');
         arr = getContinuousLocationsNorth(row, column, shipHealth);
         break;
       case 1:
-        //   console.log('East');
         arr = getContinuousLocationsEast(row, column, shipHealth);
         break;
       case 2:
-        //   console.log('South');
         arr = getContinuousLocationsSouth(row, column, shipHealth);
         break;
       case 3:
-        //   console.log('West');
-        //   console.log(row);
-        //   console.log(column);
         arr = getContinuousLocationsWest(row, column, shipHealth);
         break;
     }
@@ -95,7 +89,7 @@ const getContinuousLocationsWest = (row, column, shipHealth) => {
   } else if (endColumn > -2) {
     let startingPoint = row * 10 + column;
     let endPoint = startingPoint - shipHealth;
-    return lodash.range(startingPoint, endPoint, -1).reverse();
+    return lodash.range(startingPoint, endPoint, -1).sort();
   }
   return null;
 };
@@ -121,6 +115,82 @@ const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
 
+const getHorizontalShell = (horizontalArr) => {
+  let shellArr = [];
+  horizontalArr.forEach((position, i) => {
+    shellArr.push(horizontalBotCell(position));
+    shellArr.push(horizontalTopCell(position));
+    if (i == 0) {
+      shellArr.push(horizontalBotLeftCell(position));
+      shellArr.push(horizontalTopLeftCell(position));
+      shellArr.push(horizontalLeftCell(position));
+    } else if (i == horizontalArr.length - 1) {
+      shellArr.push(horizontalBotRightCell(position));
+      shellArr.push(horizontalTopRightCell(position));
+      shellArr.push(horizontalRightCell(position));
+    }
+  });
+
+  let finalShell = shellArr.filter((position) => position != null);
+
+  return finalShell.sort((a, b) => a - b);
+};
+const horizontalLeftCell = (position) => {
+  let leftSpot = position - 1;
+  if (leftSpot % 10 < position % 10 && leftSpot >= 0) {
+    return leftSpot;
+  }
+  return null;
+};
+const horizontalRightCell = (position) => {
+  let rightSpot = position + 1;
+  if (rightSpot % 10 > position % 10 && rightSpot < 100) {
+    return rightSpot;
+  }
+  return null;
+};
+const horizontalTopCell = (position) => {
+  let aboveSpot = position - 10;
+  if (aboveSpot >= 0) {
+    return aboveSpot;
+  }
+  return null;
+};
+const horizontalBotCell = (position) => {
+  let belowSpot = position + 10;
+  if (belowSpot < 100) {
+    return belowSpot;
+  }
+  return null;
+};
+const horizontalTopLeftCell = (position) => {
+  let aboveSpot = position - 11;
+  if (aboveSpot >= 0 && aboveSpot % 10 < position % 10) {
+    return aboveSpot;
+  }
+  return null;
+};
+const horizontalBotLeftCell = (position) => {
+  let belowSpot = position + 9;
+  if (belowSpot < 100 && belowSpot % 10 < position % 10) {
+    return belowSpot;
+  }
+  return null;
+};
+const horizontalTopRightCell = (position) => {
+  let aboveSpot = position - 9;
+  if (aboveSpot >= 0 && aboveSpot % 10 > position % 10) {
+    return aboveSpot;
+  }
+  return null;
+};
+const horizontalBotRightCell = (position) => {
+  let belowSpot = position + 11;
+  if (belowSpot < 100 && belowSpot % 10 > position % 10) {
+    return belowSpot;
+  }
+  return null;
+};
 export {
   getContinuousLocationsEast,
   getContinuousLocationsWest,
@@ -128,4 +198,5 @@ export {
   getContinuousLocationsSouth,
   generateRandomShipLocation,
   generateAIShipLocations,
+  getHorizontalShell,
 };
--- src/test/testPositionGen.spec.js
+++ src/test/testPositionGen.spec.js
@@ -77,3 +77,57 @@ test('Testing South out of bounds ', () => {
   expect(positions).toEqual(expected);
 });
 /***************************  End of South position generation  ***********************************/
+
+/***************************  Start of Shell Testing  ***********************************/
+
+// test('Testing North out of bounds ', () => {
+//   let positions = methods.getContinuousLocationsSouth(7, 2, 4);
+
+//   let expected = null;
+
+//   expect(positions).toEqual(expected);
+// });
+
+test('Testing left shell unavailable ', () => {
+  let positions = methods.getContinuousLocationsWest(3, 3, 4);
+  let shell = methods.getHorizontalShell(positions);
+  let expected = [20, 21, 22, 23, 24, 34, 40, 41, 42, 43, 44];
+  // let expected = [30, 31, 32, 33];
+  // expect(positions).toEqual(expected);
+  expect(shell).toEqual(expected);
+});
+test('Testing right & bottom shell unavailable ', () => {
+  let positions = methods.getContinuousLocationsEast(9, 6, 4);
+  let shell = methods.getHorizontalShell(positions);
+  let expected = [85, 86, 87, 88, 89, 95];
+  // let expected = [96, 97, 98, 99];
+  // expect(positions).toEqual(expected);
+  expect(shell).toEqual(expected);
+});
+
+test('Testing left & bottom shell unavailable ', () => {
+  let positions = methods.getContinuousLocationsEast(9, 0, 4);
+  let shell = methods.getHorizontalShell(positions);
+  let expected = [80, 81, 82, 83, 84, 94];
+  // let expected = [96, 97, 98, 99];
+  // expect(positions).toEqual(expected);
+  expect(shell).toEqual(expected);
+});
+
+test('Testing left & top shell unavailable ', () => {
+  let positions = methods.getContinuousLocationsEast(0, 0, 4);
+  let shell = methods.getHorizontalShell(positions);
+  let expected = [4, 10, 11, 12, 13, 14];
+  // let expected = [96, 97, 98, 99];
+  // expect(positions).toEqual(expected);
+  expect(shell).toEqual(expected);
+});
+test('Testing right & top shell unavailable ', () => {
+  let positions = methods.getContinuousLocationsEast(0, 6, 4);
+  let shell = methods.getHorizontalShell(positions);
+  let expected = [5, 15, 16, 17, 18, 19];
+  // let expected = [96, 97, 98, 99];
+  // expect(positions).toEqual(expected);
+  expect(shell).toEqual(expected);
+});
+/***************************  End of Shell Testing  ***********************************/
